d3a6e214b9561aba32609815a74bc5f01da6443c
vikas <63798850+VikasNadikuda@users.noreply.github.com>
2024-07-09 03:54:15 +0000

    changes store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,27 @@
-import { createStore } from "redux";
-
-const counterReducer = (state = { counter: 0 }, action) => {
+import { legacy_createStore as createStore } from "redux";
+const counterReducer = (state = { counter: 0, showToggle: true }, action) => {
   if (action.type === "increment") {
     return {
-      counter: counter + 1,
+      counter: state.counter + 1,
+      showToggle: state.showToggle,
     };
   }
   if (action.type === "decrement") {
     return {
-      counter: counter - 1,
+      counter: state.counter - 1,
+      showToggle: state.showToggle,
+    };
+  }
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.payload.value,
+      showToggle: state.showToggle,
+    };
+  }
+  if (action.type === "toggleCounter") {
+    return {
+      showToggle: !state.showToggle,
+      counter: state.counter,
     };
   }
   return state;
